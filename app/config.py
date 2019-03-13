def00779877f45ece0623d915dcb12b29774c175
Pichan <pichan.v@acommerce.asia>
2019-03-13 21:32:48 +0700

    Remove it out.

--- app/config.py
+++ app/config.py
@@ -3,6 +3,6 @@ from envparse import env
 HTTP_RETRY_STATUS_CODE = env.list('HTTP_RETRY_STATUS_CODE', default=[429, 502, 503, 504], subcast=int)
 
 # Twitter API Configuration
-TWITTER_CONSUMER_KEY = env.str('TWITTER_CONSUMER_KEY', default='')
-TWITTER_CONSUMER_SECRET = env.str('TWITTER_CONSUMER_SECRET', default='')
+TWITTER_CONSUMER_K = env.str('TWITTER_CONSUMER_K', default='')
+TWITTER_CONSUMER_S = env.str('TWITTER_CONSUMER_S', default='')
 TWITTER_ROOT_URL = env.str('TWITTER_ROOT_URL', default='')
